9cb2bee7bf5f34c4fda6b4610aeeccedd8b04795
Giuseppe Marcucci <marcucci.dev@gmail.com>
2020-11-28 17:41:42 +0100

    ex173 complited!

--- ex173.py
+++ ex173.py
@@ -2,27 +2,19 @@
 
 The Python Workbook, 2nd Edition
 by Ben Stephenson
-http://thinkpython2.com
 
 Copyright 2020 Giuseppe Marcucci
 
 License: http://creativecommons.org/licenses/by/4.0/
 """
 
-total = 0.0
-
 
 def total_the_values():
-    global total
-    end_input = False
     input_line: str = input("Insert a value (blank line to finish):")
-    print('input_line *'+input_line+'*')
     if input_line == '':
-        return total
+        return 0.0
     else:
-        total = 1
-        total += total_the_values()
-        return total
+        return float(input_line) + total_the_values()
 
 
 res = total_the_values()
